d1f3fbc872be15533812d3a2da0dcb67846a1450
ksrichard <bertok.richard.94@gmail.com>
2025-01-23 12:37:34 +0100

    move component updated event addition to a common place, so every modification will be recorded

--- dan_layer/engine/src/runtime/working_state.rs
+++ dan_layer/engine/src/runtime/working_state.rs
@@ -201,6 +201,16 @@ impl WorkingState {
 
         self.validate_component_state(Some(&before), &after)?;
 
+        // add event to indicate that there is a change in component
+        let (template_address, module_name) = self.current_template().map(|(addr, name)| (*addr, name.to_string()))?;
+        self.push_event(Event::new(
+            Some(locked.address().clone()),
+            template_address,
+            self.transaction_hash(),
+            "std.component.updated".to_string(),
+            tari_template_lib::models::Metadata::from([("module_name".to_string(), module_name)]),
+        ));
+
         Ok(())
     }
 

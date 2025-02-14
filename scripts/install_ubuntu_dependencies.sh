5439fd13de6bfb3d4748c302fe8252ee264dfb03
ksrichard <bertok.richard.94@gmail.com>
2025-02-14 12:54:34 +0100

    added openssl3 installation

--- scripts/install_ubuntu_dependencies.sh
+++ scripts/install_ubuntu_dependencies.sh
@@ -20,3 +20,18 @@ apt-get install --no-install-recommends --assume-yes \
   libudev-dev \
   libhidapi-dev \
   zip
+
+wget https://github.com/openssl/openssl/releases/download/openssl-3.0.8/openssl-3.0.8.tar.gz
+tar xvf openssl-3.0.8.tar.gz
+cd openssl-3.0*/
+./config
+make
+make test
+make install
+ldconfig
+tee /etc/profile.d/openssl.sh<<EOF
+export PATH=/usr/local/openssl/bin:\$PATH
+export LD_LIBRARY_PATH=/usr/local/openssl/lib:\$LD_LIBRARY_PATH
+EOF
+source /etc/profile.d/openssl.sh
+openssl version

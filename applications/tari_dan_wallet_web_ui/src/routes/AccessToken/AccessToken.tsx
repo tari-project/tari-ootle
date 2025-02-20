60a547ad6ed006270fbe19855682598b32aa7670
ksrichard <bertok.richard.94@gmail.com>
2025-02-20 11:10:07 +0100

    made access token page normal looking

--- applications/tari_dan_wallet_web_ui/src/routes/AccessToken/AccessToken.tsx
+++ applications/tari_dan_wallet_web_ui/src/routes/AccessToken/AccessToken.tsx
@@ -3,15 +3,25 @@
 
 import useAuthStore from "../../store/authStore";
 import Grid from "@mui/material/Grid";
+import CopyToClipboard from "../../Components/CopyToClipboard";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField/TextField";
 
 function AccessToken() {
     const {authToken} = useAuthStore();
 
     return (
         <>
-            <Grid container xs={6} md={6} lg={6}>
-                <Grid item xs={6} md={6} lg={6}>
-                    {authToken}
+            <Grid container
+                  alignItems="center"
+                  justifyContent="center"
+            >
+                <Grid item xs={11} sm={11} md={11} lg={11}>
+                    <Typography>Your token:</Typography>
+                    <TextField disabled multiline={true} value={authToken} style={{width: "100%", height:"10vh"}} />
+                </Grid>
+                <Grid item xs={1} sm={1} md={1} lg={1}>
+                    <CopyToClipboard title="Copy token" copy={authToken} iconWidth="25px" iconHeight="25px" />
                 </Grid>
             </Grid>
         </>

8de2ea21ecffcea21c1ccfc99b26b8034fb48e98
Stan Bondi <sdbondi@users.noreply.github.com>
2025-04-16 12:44:34 +0400

    fix: remove moon, fix npm publish

--- utilities/db_inspector/build.rs
+++ utilities/db_inspector/build.rs
@@ -14,7 +14,6 @@ fn main() {
 
     println!("cargo:rerun-if-changed=web_ui/src");
     println!("cargo:rerun-if-changed=web_ui/package.json");
-    println!("cargo:rerun-if-changed=web_ui/moon.yml");
     if let Err(e) = run() {
         // We never want to fail the build if the build fails for this utility
         println!("cargo:warning=Web UI build failed: {e}");
@@ -22,17 +21,18 @@ fn main() {
 }
 fn run() -> Result<(), Box<dyn std::error::Error>> {
     #[cfg(target_os = "windows")]
-    const MOON_BIN: &str = "moon.cmd";
+    const BIN: &str = "pnpm.cmd";
     #[cfg(not(target_os = "windows"))]
-    const MOON_BIN: &str = "moon";
-    run_command(MOON_BIN, &["db-inspector:install"])?;
-    run_command(MOON_BIN, &["db-inspector:build"])?;
+    const BIN: &str = "pnpm";
+    run_command(BIN, &["install", "--frozen-lockfile"])?;
+    run_command(BIN, &["run", "build"])?;
 
     Ok(())
 }
 
 fn run_command(command: &str, args: &[&str]) -> Result<(), Box<dyn std::error::Error>> {
     let output = std::process::Command::new(command)
+        .current_dir("./web_ui")
         .args(args)
         .output()
         .map_err(|e| format!("Failed to execute command (is {command} installed?): {e}"))?;

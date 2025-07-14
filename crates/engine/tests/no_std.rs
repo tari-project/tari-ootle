d55670e57f1ad8822e0f0228b7e8b99469bdf1e0
Stan Bondi <sdbondi@users.noreply.github.com>
2025-07-14 14:54:27 +0200

    fix: test verifying no_std works (#1510)
    Description
    ---
    test: test verifying no_std works
    fix: exclude feature std in abi and types libs unless std is used in
    template_lib
    
    Motivation and Context
    ---
    
    no_std counter example drops about 200kb (very roughly, conditions apply
    etc)
    
    How Has This Been Tested?
    ---
    New test
    
    What process can a PR reviewer use to test or verify this change?
    ---
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory to be deleted
    - [ ] Other - Please specify

---
+++ crates/engine/tests/no_std.rs
@@ -0,0 +1,39 @@
+//   Copyright 2025 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
+use tari_engine_types::indexed_value::IndexedValue;
+use tari_template_lib::models::ComponentAddress;
+use tari_template_test_tooling::TemplateTest;
+use tari_transaction::{args, Transaction};
+
+#[test]
+fn it_compiles_when_using_no_std() {
+    let mut test = TemplateTest::new(["tests/templates/no_std"]);
+
+    let access_rules_template = test.get_template_address("NoStdCounter");
+
+    let result = test.execute_expect_success(
+        Transaction::builder()
+            .allocate_component_address("no_std_counter")
+            .call_function(access_rules_template, "with_address", args![Workspace(
+                "no_std_counter"
+            )])
+            .call_method("no_std_counter", "increment", args![])
+            .build_and_seal(test.secret_key()),
+        // Because we deny_all on deposits, we need to supply the owner proof to be able to deposit the initial
+        // tokens into the new vaults
+        vec![test.owner_proof()],
+    );
+
+    let component_address: ComponentAddress = result.finalize.execution_results[1].decode().unwrap();
+
+    let component = test.read_only_state_store().get_component(component_address).unwrap();
+
+    let value: u128 = IndexedValue::from_value(component.into_state())
+        .unwrap()
+        .get_value("$.value")
+        .unwrap()
+        .unwrap();
+
+    assert_eq!(value, 1);
+}

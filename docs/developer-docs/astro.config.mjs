91a89ead08d0817f53ff1039e9ab99b645146138
Shannon Tenner <shanimal@tari.com>
2026-03-02 17:48:10 +0200

    wip: theming

--- docs/developer-docs/astro.config.mjs
+++ docs/developer-docs/astro.config.mjs
@@ -1,47 +1,45 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tari-project.github.io',
-	base: '/tari-ootle',
-	integrations: [
-		starlight({
-			favicon: '/favicon.png',
-			title: 'Tari Ootle Documentation',
-			customCss: ['./src/styles/global.scss','./src/styles/custom.scss','./src/fonts/font-face.css'],
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/tari-project/tari-ootle' }],
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Overview', link: '/introduction/' },
-					],
-				},
-				{
-					label: 'Guides',
-					items: [
-						{ label: 'Getting Started', link: '/guides/getting-started/' },
-						{ label: 'Setup a Wallet', link: '/guides/setup-a-wallet/' },
-						{ label: 'Templates Overview', link: '/guides/template-overview/' },
-						{ label: 'Building a Guessing Game', link: '/guides/build-a-guessing-game/' },
-						{ label: 'Publish the Guessing Game', link: '/guides/publishing-templates/' },
-						{ label: 'Play the Guessing Game', link: '/guides/play-the-guessing-game/' },
-						{ label: 'Transaction Overview', link: '/guides/transaction-overview/' },
-						{ label: 'Tari Cli', link: '/guides/cli/' },
-						{ label: 'Resources', link: '/guides/resources/' },
-						{ label: 'Authorization and Access', link: '/guides/authorization-and-access/' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			components: {
-				Pagination: './src/components/Pagination.astro',
-			}
-		}),
-	],
+  site: "https://tari-project.github.io",
+  base: "/tari-ootle",
+  integrations: [
+    starlight({
+      favicon: "/favicon.png",
+      title: "Tari Ootle Documentation",
+      customCss: ["./src/styles/global.scss", "./src/styles/custom.scss", "./src/fonts/font-face.css"],
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/tari-project/tari-ootle" }],
+      sidebar: [
+        {
+          label: "Introduction",
+          items: [{ label: "Overview", link: "/introduction/" }],
+        },
+        {
+          label: "Guides",
+          items: [
+            { label: "Getting Started", link: "/guides/getting-started/" },
+            { label: "Setup a Wallet", link: "/guides/setup-a-wallet/" },
+            { label: "Templates Overview", link: "/guides/template-overview/" },
+            { label: "Building a Guessing Game", link: "/guides/build-a-guessing-game/" },
+            { label: "Publish the Guessing Game", link: "/guides/publishing-templates/" },
+            { label: "Play the Guessing Game", link: "/guides/play-the-guessing-game/" },
+            { label: "Transaction Overview", link: "/guides/transaction-overview/" },
+            { label: "Tari Cli", link: "/guides/cli/" },
+            { label: "Resources", link: "/guides/resources/" },
+            { label: "Authorization and Access", link: "/guides/authorization-and-access/" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      components: {
+        Pagination: "./src/components/Pagination.astro",
+      },
+    }),
+  ],
 });

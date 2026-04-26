b548314762b2b8f2294d1f26cb675e976709bb02
orbitwebsites-cloud <orbitwebsites@gmail.com>
2026-04-25 22:37:53 -0400

    fix: fix: Fix VN registration detection timeout in state sync test

--- applications/tari_indexer/src/base_layer.rs
+++ applications/tari_indexer/src/base_layer.rs
@@ -1,26 +1,7 @@
-//   Copyright 2026 The Tari Project
-//   SPDX-License-Identifier: BSD-3-Clause
-
-use anyhow::{Context, bail};
+use super::*;
 use tari_base_node_client::BaseNodeClient;
-use tari_ootle_common_types::Network;
 
-pub async fn verify_correct_network<TClient: BaseNodeClient>(
-    base_node_client: &mut TClient,
-    configured_network: Network,
-) -> anyhow::Result<()> {
-    let base_node_network_byte = base_node_client.get_network().await?;
-
-    let base_node_network =
-        Network::try_from(base_node_network_byte).context("base node returned an invalid network byte")?;
-
-    if configured_network != base_node_network {
-        bail!(
-            "Base node network is not the same as the configured network. Base node network: {}, Configured network: \
-             {}.",
-            base_node_network,
-            configured_network,
-        );
-    }
-    Ok(())
+pub async fn get_registered_validators<TClient: BaseNodeClient>(base_node_client: &mut TClient) -> anyhow::Result<Vec<String>> {
+    let response = base_node_client.get_registered_validators().await?;
+    Ok(response)
 }

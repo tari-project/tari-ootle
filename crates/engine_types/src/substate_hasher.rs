caea7842e52f47fedcb4cc31d7aaaae1329a9c2d
Stan Bondi <sdbondi@users.noreply.github.com>
2026-04-29 12:05:39 +0400

    split a few more large substate parts in hasher

--- crates/engine_types/src/substate_hasher.rs
+++ crates/engine_types/src/substate_hasher.rs
@@ -2,10 +2,11 @@
 //   SPDX-License-Identifier: BSD-3-Clause
 
 use borsh::{BorshSerialize, io};
+use tari_template_lib::types::Hash32;
 
 use crate::{
     ProtocolVersion,
-    component::{Component, ComponentBody, ComponentHeader},
+    component::Component,
     confidential::ClaimedOutputTombstone,
     hashing::{EngineHashDomainLabel, hasher32},
     non_fungible::NonFungibleContainer,
@@ -61,27 +62,19 @@ impl<'a> From<&'a SubstateValue> for SubstateValueHashMessageV0<'a> {
 }
 
 #[derive(Debug, Clone, Copy)]
-pub struct ComponentHashMessage<'a> {
-    pub header: &'a ComponentHeader,
-    pub body: &'a ComponentBody,
-}
+pub struct ComponentHashMessage<'a>(pub &'a Component);
 
 impl<'a> From<&'a Component> for ComponentHashMessage<'a> {
     fn from(component: &'a Component) -> Self {
-        Self {
-            header: &component.header,
-            body: &component.body,
-        }
+        Self(component)
     }
 }
 
 impl borsh::BorshSerialize for ComponentHashMessage<'_> {
     fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
-        BorshSerialize::serialize(&self.header, writer)?;
+        BorshSerialize::serialize(&self.0.header, writer)?;
         // Split the body hash so that the body could be pruned
-        let body_hash = hasher32(EngineHashDomainLabel::SubstateValue)
-            .chain(&self.body)
-            .result();
+        let body_hash = hash(&self.0.body);
         BorshSerialize::serialize(&body_hash, writer)?;
 
         Ok(())
@@ -89,50 +82,42 @@ impl borsh::BorshSerialize for ComponentHashMessage<'_> {
 }
 
 #[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct ResourceHashMessage<'a> {
-    pub resource: &'a Resource,
-}
+pub struct ResourceHashMessage<'a>(pub &'a Resource);
 
 impl<'a> From<&'a Resource> for ResourceHashMessage<'a> {
     fn from(resource: &'a Resource) -> Self {
-        Self { resource }
+        Self(resource)
     }
 }
 
 #[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct VaultHashMessage<'a> {
-    pub vault: &'a Vault,
-}
+pub struct VaultHashMessage<'a>(&'a Vault);
 
 impl<'a> From<&'a Vault> for VaultHashMessage<'a> {
     fn from(vault: &'a Vault) -> Self {
-        Self { vault }
+        Self(vault)
     }
 }
 
 #[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct NonFungibleContainerHashMessage<'a> {
-    pub non_fungible: &'a NonFungibleContainer,
-}
+pub struct NonFungibleContainerHashMessage<'a>(&'a NonFungibleContainer);
 
 impl<'a> From<&'a NonFungibleContainer> for NonFungibleContainerHashMessage<'a> {
     fn from(non_fungible: &'a NonFungibleContainer) -> Self {
-        Self { non_fungible }
+        Self(non_fungible)
     }
 }
 
 #[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct ClaimedOutputTombstoneHashMessage<'a> {
-    pub tombstone: &'a ClaimedOutputTombstone,
-}
+pub struct ClaimedOutputTombstoneHashMessage<'a>(&'a ClaimedOutputTombstone);
 
 impl<'a> From<&'a ClaimedOutputTombstone> for ClaimedOutputTombstoneHashMessage<'a> {
     fn from(tombstone: &'a ClaimedOutputTombstone) -> Self {
-        Self { tombstone }
+        Self(tombstone)
     }
 }
 
-#[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
+#[derive(Debug, Clone, Copy)]
 pub struct TransactionReceiptHashMessage<'a> {
     pub receipt: &'a TransactionReceipt,
 }
@@ -143,7 +128,22 @@ impl<'a> From<&'a TransactionReceipt> for TransactionReceiptHashMessage<'a> {
     }
 }
 
-#[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
+impl borsh::BorshSerialize for TransactionReceiptHashMessage<'_> {
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+        BorshSerialize::serialize(&self.receipt.outcome, writer)?;
+        BorshSerialize::serialize(&self.receipt.diff_summary, writer)?;
+        BorshSerialize::serialize(&self.receipt.fee_withdrawals, writer)?;
+        let events = hash(&self.receipt.events);
+        BorshSerialize::serialize(&events, writer)?;
+        let logs = hash(&self.receipt.logs);
+        BorshSerialize::serialize(&logs, writer)?;
+        BorshSerialize::serialize(&self.receipt.fee_receipt, writer)?;
+        BorshSerialize::serialize(&self.receipt.epoch, writer)?;
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct PublishedTemplateHashMessage<'a> {
     pub template: &'a PublishedTemplate,
 }
@@ -154,24 +154,39 @@ impl<'a> From<&'a PublishedTemplate> for PublishedTemplateHashMessage<'a> {
     }
 }
 
-#[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct ValidatorFeePoolHashMessage<'a> {
-    pub pool: &'a ValidatorFeePool,
+impl borsh::BorshSerialize for PublishedTemplateHashMessage<'_> {
+    fn serialize<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+        BorshSerialize::serialize(&self.template.template_name, writer)?;
+        BorshSerialize::serialize(&self.template.at_epoch, writer)?;
+        BorshSerialize::serialize(&self.template.author, writer)?;
+        BorshSerialize::serialize(&self.template.metadata_hash, writer)?;
+
+        let binary_hash = hash(&self.template.binary);
+        BorshSerialize::serialize(&binary_hash, writer)?;
+        Ok(())
+    }
 }
+
+#[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
+pub struct ValidatorFeePoolHashMessage<'a>(&'a ValidatorFeePool);
 
 impl<'a> From<&'a ValidatorFeePool> for ValidatorFeePoolHashMessage<'a> {
     fn from(pool: &'a ValidatorFeePool) -> Self {
-        Self { pool }
+        Self(pool)
     }
 }
 
 #[derive(Debug, Clone, Copy, borsh::BorshSerialize)]
-pub struct UtxoHashMessage<'a> {
-    pub utxo: &'a Utxo,
-}
+pub struct UtxoHashMessage<'a>(&'a Utxo);
 
 impl<'a> From<&'a Utxo> for UtxoHashMessage<'a> {
     fn from(utxo: &'a Utxo) -> Self {
-        Self { utxo }
+        Self(utxo)
     }
+}
+
+fn hash<T: borsh::BorshSerialize>(value: &T) -> Hash32 {
+    hasher32(EngineHashDomainLabel::SubstateValuePart)
+        .chain(&value)
+        .result()
 }

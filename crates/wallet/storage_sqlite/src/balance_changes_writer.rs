aec807f93bdcb99292241f2297688de855a117e0
BWM0223 <b.anthony9618@gmail.com>
2026-06-26 23:36:03 -0700

    fix: add SPDX license header to balance_changes_writer.rs

--- crates/wallet/storage_sqlite/src/balance_changes_writer.rs
+++ crates/wallet/storage_sqlite/src/balance_changes_writer.rs
@@ -1,3 +1,6 @@
+//   Copyright 2024 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
 use diesel::prelude::*;
 use crate::models::balance_change::{BalanceChange, BalanceChangeSource};
 

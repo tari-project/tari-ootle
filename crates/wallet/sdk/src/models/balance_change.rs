26eafdb12be061bffb5f476dfe6765a5c047e2a3
BWM0223 <b.anthony9618@gmail.com>
2026-06-26 23:36:02 -0700

    fix: add SPDX license header to balance_change.rs

--- crates/wallet/sdk/src/models/balance_change.rs
+++ crates/wallet/sdk/src/models/balance_change.rs
@@ -1,3 +1,6 @@
+//   Copyright 2024 The Tari Project
+//   SPDX-License-Identifier: BSD-3-Clause
+
 use chrono::NaiveDateTime;
 use tari_ootle_transaction::transaction_id::TransactionId;
 use tari_template_lib::models::{Amount, ComponentAddress, ResourceAddress, VaultId};
